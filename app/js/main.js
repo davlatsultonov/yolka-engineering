e2dd66f28ba0cc4ab900ffbd1f7eeeae6ddc0c5e
davlatsultonov <davlatsultonov2001@gmail.com>
2022-04-20 21:48:49 +0500

    script refactoring
    - added slider to banners
    - changed back-drop behavior
    - style fixes

--- app/js/main.js
+++ app/js/main.js
@@ -1,28 +1,45 @@
 $(document).ready(function(){
+    // sliders
     const $slider = $('.slider');
     const $sliderProject = $('.slider--project');
+    const $sliderBanner = $('.slider-banner');
     const $isSliderProject = $sliderProject.length;
-    const $hasSliderChildren = $slider.children().length > 1;
-    $slider.owlCarousel({
-        loop: true,
-        nav: $hasSliderChildren,
-        dots: $hasSliderChildren && !$isSliderProject,
-        items: 1,
-        navText: $hasSliderChildren ? [`<img alt='slider-arrow-icon' src='images/icons/slider_arrow-left${$isSliderProject ? '--green' : ''}.svg'>`, `<img alt='slider-arrow-icon' src='images/icons/slider_arrow-right${$isSliderProject ? '--green' : ''}.svg'>`] : [],
-        navContainer: `#slider${$isSliderProject ? '-project' : ''}-nav`,
-        dotsContainer: $isSliderProject ? false : '#slider-dots',
-        autoHeight:true,
-        touchDrag: $hasSliderChildren,
-        mouseDrag: $hasSliderChildren,
-    })
 
-    $sliderProject.find('.slider__title').each((i, item) => {
-        $(item).css('min-width', $(window).width() > 640 ? $(item).width() + 70 : '100%');
-    })
+    if ($slider.length || $isSliderProject) {
+        const $hasSliderChildren = $slider.find('.slider__item').length > 1;
 
-    if ($hasSliderChildren) $('#slider-nav').width($('#slider-dots').width() + ($(window).width() < 480 ? 120 : 160)).height($('#slider-dots').height())
+        $slider.owlCarousel({
+            ...baseSliderOptions($slider, $hasSliderChildren),
+            navText: $hasSliderChildren ? [`<img alt='slider-arrow-icon' src='images/icons/slider_arrow-left${$isSliderProject ? '--green' : ''}.svg'>`, `<img alt='slider-arrow-icon' src='images/icons/slider_arrow-right${$isSliderProject ? '--green' : ''}.svg'>`] : [],
+            navContainer: `#slider${$isSliderProject ? '-project' : ''}-nav`,
+            dots: $hasSliderChildren && !$isSliderProject,
+            dotsContainer: $isSliderProject ? false : '#slider-dots',
+        })
 
-    $(document).on('click', '#hamburger-btn', function () {
+        $sliderProject.find('.slider__title').each((i, item) => {
+            $(item).css('min-width', $(window).width() > 640 ? $(item).width() + 70 : '100%');
+        })
+
+        if ($hasSliderChildren) setDynamicWidthToSliderNav();
+    }
+
+    if ($sliderBanner.length) {
+        const $hasSliderChildren = $sliderBanner.find('.banner__img').length > 1;
+
+        $sliderBanner.owlCarousel({
+            ...baseSliderOptions($sliderBanner, $hasSliderChildren),
+            navContainer: '#slider-nav',
+            dotsContainer: '#slider-dots',
+        })
+
+        if ($hasSliderChildren) setDynamicWidthToSliderNav();
+    }
+
+    setBlurredBackground('.slider__img', '.slider-hash-navigation__img', '.banner__img');
+    // sliders end
+
+    $(document).on('click', '[data-id="hamburger-btn"]', function () {
+        console.log(this);
         $('#side-menu').toggleClass('active');
     })
 
@@ -30,8 +47,8 @@ $(document).ready(function(){
         $('#side-menu').removeClass('active');
     })
 
-    $(document).on('click', function (e) {
-        if (!e.target.closest('#side-menu') && !e.target.closest('#hamburger-btn')) $('#side-menu').removeClass('active');
+    $(document).on('click', '.back-drop', function () {
+        $(this).parent().removeClass('active')
     })
 
     // video
@@ -45,3 +62,62 @@ $(document).ready(function(){
         $(this).remove()
     })
 });
+
+function setDynamicWidthToSliderNav() {
+    const sliderDots = $('#slider-dots');
+    const delta = $(window).width() < 480 ? 120 : 160;
+
+    $('#slider-nav')
+        .width(sliderDots.width() + delta)
+        .height(sliderDots.height());
+}
+
+function baseSliderOptions(slider, hasSliderChildren) {
+    return {
+        loop: true,
+        items: 1,
+        autoHeight:true,
+        nav: hasSliderChildren,
+        navText: hasSliderChildren ? [`<img alt='slider-arrow-icon' src='images/icons/slider_arrow-left.svg'>`, `<img alt='slider-arrow-icon' src='images/icons/slider_arrow-right.svg'>`] : [],
+        touchDrag: hasSliderChildren,
+        mouseDrag: hasSliderChildren,
+        dots: hasSliderChildren,
+    }
+}
+
+function setBlurredBackground(...images) {
+    if (!images.length) return;
+
+    images.forEach(img => {
+        const imageItems = document.querySelectorAll(`${img}`);
+
+        if (!imageItems.length) return;
+
+        imageItems.forEach((el) => {
+            const childEl = $(el).find('.blurred-img__front')[0];
+
+            if (!childEl) return;
+
+            const imgSrc = childEl.style.backgroundImage.replace(/url\((['"])?(.*?)\1\)/gi, '$2').split(',')[0];
+            const newImage = new Image();
+            newImage.src = imgSrc;
+            newImage.onload = function () {
+                const width = newImage.width;
+                const height = newImage.height;
+                const portrait = width < height;
+
+                childEl.style.backgroundSize = (portrait) ? 'contain' : 'cover';
+
+                if (portrait) $(createBlurredImgBack(imgSrc)).insertAfter(childEl)
+            };
+        });
+    })
+}
+
+function createBlurredImgBack(imgSrc) {
+    let div = document.createElement('div');
+    div.classList.add('blurred-img__back');
+    div.style.backgroundImage = `url(${imgSrc})`;
+
+    return div;
+}
